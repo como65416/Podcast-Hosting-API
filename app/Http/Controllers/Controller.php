f4cad4c5a7da9cece4d2339281bc77af25658584
comoco <como65416@gmail.com>
2020-10-08 22:28:53 +0800

    Base Controller add validateRequest function

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\BadRequestException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    /**
+     * @param  Request $request
+     * @param  array   $rules   laravel validate rules
+     *
+     * @throws \App\Exceptions\BadRequestException if any rule is not validated
+     */
+    protected function validateRequest(Request $request, array $rules)
+    {
+        $validator = Validator::make($request->all(), $rules);
+
+        $errors = $validator->errors();
+        if ($errors->any()) {
+            throw (new BadRequestException('input not validated'))->setErrors($errors->all());
+        }
+    }
 }
